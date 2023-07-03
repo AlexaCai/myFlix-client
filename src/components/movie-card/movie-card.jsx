187a074c48e2161d7179594f988f1a69fd000e77
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-07-03 14:57:17 -0600

    Creation of the movie-card component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,14 @@
+//***''export'' exposes the ''MovieCard'' component making it available for use by other components, modules, and files - possible to import in other files.
+//***''const MovieCard'' (and the following codes) creates the MovieCard component. The function assigned to MovieCard returns the visual representation of the component (the function renders what is displayed on the screen). Inside this function is JSX.
+//***The ''onMovieClick'' inside ({ movie, onMovieClick }) is a prop from main-view.jsx being extracted here using object destructuring.
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
-    );
-  };
+  return (
+    <div
+    //***A callback function is passed to onClick, then the logic (onMovieClick(movie);) that's needed to execute once a click event is registered is added.
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.title}</div>
+  );
+};
