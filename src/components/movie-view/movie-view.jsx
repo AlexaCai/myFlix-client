d8abca4a22e36523af6108d5b024b43357c4192e
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-07-11 20:02:47 -0600

    Modification of each movie image width

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,12 +3,13 @@ import PropTypes from "prop-types";
 
 //***''export'' keyword exposes the ''MovieView'' component making it available for use by other components, modules, and files - possible to import in other files.
 //***''const MovieView'' (and the following codes) creates the MovieView component. The function assigned to MovieView returns the visual representation of the component (the function renders what is displayed on the screen). Each information displayed in this file are taken from/linked to the main-view.jsx. For exemple, to display the title of a movie, the code <span>{movie.title}</span> is used, and this code refers to each movie object (and their title more precisely) inside the ''movies'' array present in the main-view.jsx.
+//***className="w-100"
 //***''onBackClick'' in ({ movie, onBackClick }) is exported to be used in main-view.jsx, and to add there the logic allowing to go back after clicking on a movie for more details (return to main-view and movie-cards). The code below ''<button onClick={onBackClick}>Back</button>'' calls the function prop ''onBackClick'' defined in main-view.jsx when the button click occurs.
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img className="w-50" src={movie.image} />
       </div>
       <div>
         <span>Title: </span>
