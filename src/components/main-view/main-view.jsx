2a77572b36d6827157ad5982b008548271afa12c
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-07-19 22:42:24 -0600

    Responsiveness updated for movie view component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -152,7 +152,7 @@ export const MainView = () => {
                                 ) : movies.length === 0 ? (
                                     <Col>The list is empty!</Col>
                                 ) : (
-                                    <Col md={8}>
+                                    <Col>
                                         <MovieView movies={movies} />
                                     </Col>
                                 )}
