56687cfbb444d942c512c8058dac2bdb0352ad85
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-09-16 09:09:06 -0600

    Sync test

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,5 @@
 import { createRoot } from 'react-dom/client';
-
-//***Import the MainView component. Note: MainView must be enclosed in curly braces because it was exported using the ''named export'' method in the main-view.jsx file.
 import { MainView } from './components/main-view/main-view';
-
-//***Import the Container Bootstrap component for Bootstrap grid UI design.
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
